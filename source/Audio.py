a06d9715a8c24ea6b00d6ad8a8ecae7cd8a5f13f
John_Dominic <johndominicjasmin@gmail.com>
2021-11-22 18:15:02 +0800

    Add class, add value for  draw sounds

--- source/Audio.py
+++ source/Audio.py
@@ -2,29 +2,31 @@ from PyQt5.QtCore import QCoreApplication
 from PyQt5.QtMultimedia import QSound
 import sys
 from Constants import _Constants as constants
-def playBackgroundMusic():
-   __play(constants.BACKGROUND_MUSIC)
-
-def playClickingSound():
-   __play(constants.MOUSE_CLICK)
-
-def playGameOverSound():
-   __play(constants.GAMEOVER_SOUND)
-
-def playLoseSound():
-   __play(constants.LOSE_SOUND)
-
-def playVictorySound():
-   __play(constants.VICTORY_SOUND) 
-
-def playWinSound():
-   __play(constants.WIN_SOUND)
-
-def __play(file_name):
-    app = QCoreApplication(sys.argv)
-    sound = QSound(constants.SOUND_DIRECTORY+file_name+constants.SOUND_FILE_FORMAT)
-    sound.play()
-    sys.exit(app.exec_())
 
 
-playBackgroundMusic()
+class _Audio:
+   def playBackgroundMusic(self):
+      self.__play(constants.BACKGROUND_MUSIC)
+
+   def playClickingSound(self):
+      self.__play(constants.MOUSE_CLICK)
+
+   def playGameOverSound(self):
+      self.__play(constants.GAMEOVER_SOUND)
+
+   def playLoseSound(self):
+      self.__play(constants.LOSE_SOUND)
+
+   def playDrawSound(self):
+      self.play(constants.DRAW_SOUND)
+
+   def playVictorySound(self):
+      self.__play(constants.VICTORY_SOUND) 
+
+   def playWinSound(self):
+      self.__play(constants.WIN_SOUND)
+
+   def __play(self,file_name):
+      self.sound = QSound(constants.SOUND_DIRECTORY + file_name + constants.SOUND_FILE_FORMAT)
+      self.sound.play()
+   
